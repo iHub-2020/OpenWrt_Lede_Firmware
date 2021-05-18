957cf700c78b63fc2c14c90d76b30cd27a1ddfd6
iHub-2020 <68175751+iHub-2020@users.noreply.github.com>
2021-05-18 23:15:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.198/g' package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/Ryan's OpenWrt/g' package/base-files/files/bin/config_generate
